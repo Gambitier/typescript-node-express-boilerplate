555e6c25915e3e6877c2736f9aa99114563f6deb
gambitier <csegambitier@gmail.com>
2022-03-06 16:10:07 +0530

    update boilerplate with auth

--- src/config/configuration.manager.ts
+++ src/config/configuration.manager.ts
@@ -1,17 +1,30 @@
-import { Configurations } from './configuration.types';
+import { AuthenticationType, AuthorizationType, Configurations } from './configuration.types';
+import * as AppConfig from '../app.config.json';
 
 export class ConfigurationManager {
     static _config: Configurations = null;
-
-    public static MaxUploadFileSize = (): number => {
-        return ConfigurationManager._config.MaxUploadFileSize;
-    };
 
     public static loadConfigurations = (): void => {
         ConfigurationManager._config = {
             BaseUrl: process.env.BASE_URL,
             SystemIdentifier: '',
             MaxUploadFileSize: 0,
+            Auth: {
+                Authentication: AppConfig.Auth.Authentication as AuthenticationType,
+                Authorization: AppConfig.Auth.Authorization as AuthorizationType,
+            },
         };
+    };
+
+    public static Authentication = (): AuthenticationType => {
+        return ConfigurationManager._config.Auth.Authentication;
+    };
+
+    public static Authorization = (): AuthorizationType => {
+        return ConfigurationManager._config.Auth.Authorization;
+    };
+
+    public static MaxUploadFileSize = (): number => {
+        return ConfigurationManager._config.MaxUploadFileSize;
     };
 }
